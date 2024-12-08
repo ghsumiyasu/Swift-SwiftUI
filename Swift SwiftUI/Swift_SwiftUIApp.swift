5e469d74a5dd7f4ad10fa86639f94a1a246b8f0c
ghsumiyasu <ghsumiyasu@gmail.com>
2024-12-08 21:26:07 +0900

    Initial Commit

---
+++ Swift SwiftUI/Swift_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SwiftUIApp.swift
+//  Swift SwiftUI
+//
+//  Created by Desenvolvimento on 2024/12/08.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift SwiftUITests/Swift_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SwiftUITests.swift
+//  Swift SwiftUITests
+//
+//  Created by Desenvolvimento on 2024/12/08.
+//
+
+import Testing
+@testable import Swift_SwiftUI
+
+struct Swift_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
